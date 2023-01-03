2cb5795dc05ba1bbb5bd387bedd46f4633b2346a
unknown <matheuskiller6@gmail.com>
2023-01-03 16:42:11 -0300

    lacos

---
+++ DoWhile.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+
+	  Scanner leia = new Scanner (System.in);
+      int soma=0,numero;
+      System.out.println("Digite um número: ");
+
+      do 
+          {
+          numero = leia.nextInt();
+              if (numero>0)
+              {
+                  soma = numero + soma;
+              }
+          }
+      while (numero!= 0);
+      System.out.println("\nA soma dos números digitados foi: "+soma);
+
+  }
+
+}
