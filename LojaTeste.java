59b581d261aebc765161863cbf892731d20563b3
unknown <matheuskiller6@gmail.com>
2023-01-05 21:29:27 -0300

    pooexercicios

---
+++ LojaTeste.java
@@ -0,0 +1,16 @@
+package POOexercicios;
+
+public class LojaTeste {
+
+	public static void main(String[] args) {
+
+		LojaDeGames console1 = new LojaDeGames("X-box series-X","2Tb","1 Controle","Incluso GTA-V","= 3.200");
+			console1.imprintInfo();
+		LojaDeGames console2 = new LojaDeGames("PS5","1Tb","2 Controle","Jogo incluso FreeFire","= 4.800");
+			console2.imprintInfo();
+			
+		
+	}
+	
+
+}
